1b5cbb035e57f3bdf8891e7d3065345bc5e98fb0
Noodl3r <echelon.r6s@gmail.com>
2025-07-11 23:00:30 -0400

    More basic fixes

--- init.lua
+++ init.lua
@@ -2,6 +2,10 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.env.MESA_NO_ERROR = "1"
+vim.env.MESA_GLTHREAD = "false"
+
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -34,6 +38,7 @@ require("lazy").setup({
 })
 
 vim.cmd([[colorscheme pywal]])
+
 
 -- Basic Neovim settings
 require("basicsettings")
